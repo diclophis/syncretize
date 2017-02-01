4e88c46fbe89e732c50f1192a16a665d2f9298c6
Jon Bardin <diclophis@gmail.com>
2017-02-01 11:10:58 +0000

    make sure only bin script is active

--- lib/syncretizer.rb
+++ lib/syncretizer.rb
@@ -4,49 +4,49 @@ require 'yaml'
 require 'psych'
 require 'uri'
 
-# "$(REPO)" $(IMAGE_NAME) $(IMAGE_TAG)
+if File.basename($0) == "syncretizer"
+  rc_yml_path = ARGV[0] || raise
+  registry_domain = ARGV[1] || raise
+  image_name = ARGV[2] || raise
+  image_tag = ARGV[3] || raise
 
-rc_yml_path = ARGV[0] || raise
-registry_domain = ARGV[1] || raise
-image_name = ARGV[2] || raise
-image_tag = ARGV[3] || raise
+  class DocumentStreamHandler < Psych::TreeBuilder
+    def initialize &block
+      super
+      @block = block
+    end
 
-class DocumentStreamHandler < Psych::TreeBuilder
-  def initialize &block
-    super
-    @block = block
+    def end_document implicit_end = !streaming?
+      @last.implicit_end = implicit_end
+      @block.call pop
+    end
+
+    def start_document version, tag_directives, implicit
+      n = Psych::Nodes::Document.new version, tag_directives, implicit
+      push n
+    end
   end
 
-  def end_document implicit_end = !streaming?
-    @last.implicit_end = implicit_end
-    @block.call pop
+  document_handler_switch = Proc.new do |document|
+    add_to_pending_documents = false
+
+    description = document.to_ruby
+    kind = description["kind"]
+    name = description["metadata"]["name"]
+
+    case kind
+      when "Service"
+
+      when "ReplicationController"
+        description["spec"]["template"]["spec"]["containers"][0]["image"] = (registry_domain + (image_name + ":" + image_tag))
+
+    else
+    end
+
+    puts description.to_yaml
   end
 
-  def start_document version, tag_directives, implicit
-    n = Psych::Nodes::Document.new version, tag_directives, implicit
-    push n
-  end
+  handler = DocumentStreamHandler.new(&document_handler_switch)
+  parser = Psych::Parser.new(handler)
+  parser.parse(File.read(rc_yml_path), rc_yml_path)
 end
-
-document_handler_switch = Proc.new do |document|
-  add_to_pending_documents = false
-
-  description = document.to_ruby
-  kind = description["kind"]
-  name = description["metadata"]["name"]
-
-  case kind
-    when "Service"
-
-    when "ReplicationController"
-      description["spec"]["template"]["spec"]["containers"][0]["image"] = (registry_domain + (image_name + ":" + image_tag))
-
-  else
-  end
-
-  puts description.to_yaml
-end
-
-handler = DocumentStreamHandler.new(&document_handler_switch)
-parser = Psych::Parser.new(handler)
-parser.parse(File.read(rc_yml_path), rc_yml_path)
